38a3bc3273337e1be0b8160de1ea9439c090f814
ausi1997.com <ausafen1018@gmail.com>
2021-06-14 07:59:04 +0530

    mapping through api data

--- src/components/coin.js
+++ src/components/coin.js
@@ -1,17 +1,24 @@
 import React from "react";
 
-const Coin =()=>{
+const Coin =({name,price,symbol,image,volume,priceChange,marketCap})=>{
     return(
         <div className="coin-container">
         <div className="coin-row">
         <div className="coin">
-        <img src={} alt='loading...'></img>
-        <h1>{}</h1>
-        <p className="coin-symbol">{}</p>
+        <img src={image} alt='loading...'></img>
+        <h1>{name}</h1>
+        <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-        <p className="coin-price">{}</p>
-        <p className='coin-volume'>{}</p>
+        <p className="coin-price">{price}</p>
+        <p className='coin-volume'>{volume}</p>
+        {
+         priceChange<0 ?(
+             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+         ):
+        ( <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+         ) }
+         <p className='coin-marketcap'>{marketCap}</p>
         </div>
         </div>
         </div>
--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Coin from "./coin";
 const uri = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 
 const Main = ()=>{
 
     const [coins, setCoins] = useState([]);
+    const [search,setSearch] = useState('');
 
   useEffect(()=>{
     axios.get(uri)
@@ -18,6 +20,12 @@ const Main = ()=>{
     })
   },[]);
 
+  const Search = (e)=>{
+      setSearch(e.target.value)
+  }
+const SearchCoins = coins.filter(coin=>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+);
     return(
         <div className="coin-app">
         <div className ="coin-search">
@@ -27,9 +35,25 @@ const Main = ()=>{
          type="text"
          placeholder="Search"
          className="coin-input"
+         onChange={Search}
         ></input>
         </form>
         </div>
+        {
+            SearchCoins.map(item=>{
+                return(
+                    <Coin key={item.id} 
+                     name={item.name}
+                     image={item.image}
+                     symbol={item.symbol}
+                     marketCap={item.market_cap}
+                     price={item.current_price}
+                     priceChange={item.price_change_percentage_24h}
+                     volume={item.total_volume}
+                    ></Coin>
+                )
+            })
+        }
         </div>
     )
 }
